c4d2a41c47dd73e2151246754d9d3c432b1daddc
edgar <edgar@mottimotti.com>
2013-10-25 13:25:32 +0300

    fixing duplicates after copy

--- lib/issue_add_in_project_count.rb
+++ lib/issue_add_in_project_count.rb
@@ -1,8 +1,7 @@
 class IssueObserver < ActiveRecord::Observer
   def after_save(issue)
-    if issue.in_project_count == 0
-      issue.in_project_count = Issue.where(:project_id => issue.project_id).sort_by(&:in_project_count).last.in_project_count + 1
-      issue.save
+    if issue.in_project_count == 0 || Issue.where(:project_id => issue.project_id).map(&:in_project_count).include?(issue.in_project_count)
+      issue.update_column(:in_project_count, Issue.where(:project_id => issue.project_id).sort_by(&:in_project_count).last.in_project_count + 1)
     end
   end
 end
